ba7f28c53e17c77ae3bb274e8161b54692c77966
Nicholas Weaver <lynxbat@gmail.com>
2012-02-27 14:41:52 -0600

    start script

--- lib/slices/slice_boot.rb
+++ lib/slices/slice_boot.rb
@@ -28,13 +28,17 @@ module Razor::Slice
 
     def boot_called
       if @web_command
-
-        logger.debug ""
-        # todo call engine with uuid
-        # prove out boot script can pull razor server from existing ixe var
-        # junk stub code to make ipxe boot work, calls razor image
-        puts "#!ipxe\ninitrd http://192.168.99.10:8027/razor/image/mk || reboot\nchain http://192.168.99.10:8027/razor/image/memdisk iso || reboot"
-        return
+        @command_query_string = @command_array.shift
+        if @command_query_string != "{}" && @command_query_string != nil
+          params = JSON.parse(@command_query_string)
+          mac_address = params['mac']
+          logger.debug "Boot called by Node(MAC: #{mac_address}"
+          # todo call engine with uuid
+          # prove out boot script can pull razor server from existing ixe var
+          # junk stub code to make ipxe boot work, calls razor image
+          puts "#!ipxe\ninitrd http://192.168.99.10:8027/razor/image/mk || reboot\nchain http://192.168.99.10:8027/razor/image/memdisk iso || reboot"
+          return
+        end
       end
       slice_error("NotImplemented")
     end
