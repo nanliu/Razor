5a8fc95f6e14619ef7075c7804ca3b69920b545a
Nicholas Weaver <lynxbat@gmail.com>
2012-03-06 09:11:06 -0600

    Changed Engine to be a singleton

--- lib/project_razor/engine.rb
+++ lib/project_razor/engine.rb
@@ -3,67 +3,72 @@
 
 # Used for all event-driven commands and policy resolution
 # @author Nicholas Weaver
-class ProjectRazor::Engine
-  include(ProjectRazor::Logging)
+
+require "singleton"
+
+module ProjectRazor
+  class Engine
+    include(ProjectRazor::Logging)
+    include(Singleton)
+
+    # TODO policy resolve
 
 
-  # TODO policy resolve
+    # TODO tag rules resolve
 
 
-  # TODO tag rules resolve
+    def get_boot(uuid)
+      logger.debug "Getting boot for uuid:#{uuid}"
 
 
-  def get_boot(uuid)
-    logger.debug "Getting boot for uuid:#{uuid}"
+      # Run tagging policies
+      node_tagging(uuid)
 
 
-    # Run tagging policies
-    node_tagging(uuid)
-
-
-    boot_script = ""
-    boot_script << "#!ipxe\n"
-    boot_script << "initrd http://192.168.99.10:8027/razor/image/mk\n"
-    boot_script << "chain http://192.168.99.10:8027/razor/image/memdisk iso\n"
-    boot_script
-  end
-
-
-
-  # TODO Tagging
-
-
-  def node_tags(node)
-    node.attributes_hash
-    tag_policies = $data.fetch_all_objects(:tag)
-
-    tags = []
-    tag_policies.each do
-      |tag_pol|
-      if tag_pol.check_tag_rule(node.attributes_hash)
-        tags << tag_pol.tag
-      end
+      boot_script = ""
+      boot_script << "#!ipxe\n"
+      boot_script << "initrd http://192.168.99.10:8027/razor/image/mk\n"
+      boot_script << "chain http://192.168.99.10:8027/razor/image/memdisk iso\n"
+      boot_script
     end
-    tags
+
+
+
+    # TODO Tagging
+
+
+    def node_tags(node)
+      node.attributes_hash
+      tag_policies = $data.fetch_all_objects(:tag)
+
+      tags = []
+      tag_policies.each do
+      |tag_pol|
+        if tag_pol.check_tag_rule(node.attributes_hash)
+          tags << tag_pol.tag
+        end
+      end
+      tags
+    end
+
+
+
+
+    # TODO Policy
+
+    def node_policy
+
+
+    end
+
+
+    def mk_boot
+
+
+    end
+
+
+
+
   end
-
-
-
-
-  # TODO Policy
-
-  def node_policy
-
-
-  end
-
-
-  def mk_boot
-
-
-  end
-
-
-
-
 end
