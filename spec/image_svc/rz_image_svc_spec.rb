ab00a2fe6b204bd7f8fc4dc8a932e7ddb3e42475
Nicholas Weaver <lynxbat@gmail.com>
2012-03-11 16:28:50 -0500

    Changes to image service base

--- spec/image_svc/rz_image_svc_spec.rb
+++ spec/image_svc/rz_image_svc_spec.rb
@@ -6,11 +6,16 @@ require "rspec"
 
 describe ProjectRazor::ImageService do
 
+  before(:all) do
+    @data = ProjectRazor::Data.new
+    @config = @data.config
+  end
+
 
   describe ".Microkernel" do
     it "should do something" do
       new_mk = ProjectRazor::ImageService::MicroKernel.new({})
-      resp = new_mk.add("~/Documents/rz_mk-image-dev-0.2.0.0.iso","./")
+      resp = new_mk.add("#{$razor_root}/rz_mk_dev-image.0.2.0.0.iso",@config.image_svc_path)
       p  resp
     end
 
