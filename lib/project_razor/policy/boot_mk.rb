4ec206dfc50f5a9dfc0210a799470f2b42910d72
Nicholas Weaver <lynxbat@gmail.com>
2012-03-20 20:51:50 -0500

    .

--- lib/project_razor/policy/boot_mk.rb
+++ lib/project_razor/policy/boot_mk.rb
@@ -23,8 +23,9 @@ module ProjectRazor
         image_svc_uri = "http://#{@config.image_svc_host}:#{@config.image_svc_port}/razor/image"
         boot_script = ""
         boot_script << "#!ipxe\n"
-        boot_script << "initrd #{image_svc_uri}/mk || goto error\n"
-        boot_script << "chain #{image_svc_uri}/memdisk iso || goto error\n"
+        boot_script << "kernel #{image_svc_uri}/mk/kernel || goto error\n"
+        boot_script << "initrd #{image_svc_uri}/mk/initrd || goto error\n"
+        boot_script << "boot || goto error\n"
         boot_script << "\n\n\n"
         boot_script << ":error\necho ERROR, will reboot in #{@config.mk_checkin_interval}\nsleep #{@config.mk_checkin_interval}\nreboot\n"
         boot_script
