0bb319741dfb3bea0d699c19143f389a1170f189
Nicholas Weaver <lynxbat@gmail.com>
2012-03-20 21:19:16 -0500

    header setting out of order

--- bin/image_svc.js
+++ bin/image_svc.js
@@ -33,9 +33,11 @@ app.get('/razor/image/mk*',
 function respondWithFile(path, res) {
     if (path != null) {
         var filename = path.split("/")[path.split("/").length - 1];
-        res.writeHead(200, {'Content-Type': 'application/octet-stream'});
-        var fileStream = fs.createReadStream(path);
+
         res.setHeader('Content-disposition', 'attachment; filename=' + filename);
+        res.writeHead(200, {'Content-Type': 'application/octet-stream'});
+
+        var fileStream = fs.createReadStream(path);
         fileStream.on('data', function(chunk) {
             res.write(chunk);
         });
