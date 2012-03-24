d32731ffba25575cd0831ce93b949b590432a80a
Nicholas Weaver <lynxbat@gmail.com>
2012-03-24 16:43:59 -0500

    better feedback for model config population

--- lib/project_razor/slice/policy.rb
+++ lib/project_razor/slice/policy.rb
@@ -117,12 +117,16 @@ module ProjectRazor
 
         @model_config_uuid = @command_array.shift
         unless @model_config_uuid != nil
+          @command_array.unshift(@policy_type_name)
+          print_model_config get_model_config
           slice_error("MustProvideModelConfigUUID")
           return
         end
         setup_data
         @model_config = @data.fetch_object_by_uuid(:model, @model_config_uuid)
         unless @model_config != nil
+          @command_array.unshift(@policy_type_name)
+          print_model_config get_model_config
           slice_error("MustProvideModelConfigUUID")
           return
         end
