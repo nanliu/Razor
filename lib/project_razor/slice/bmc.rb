9d0ef1c704121fab5917c7f65a3cab992e07dd4c
Tom McSweeney <tjmcs@coastaccess.com>
2012-03-24 18:24:45 -0700

    Added new commands to the Bmc object/slice so that it can interact with the IpmiController to control the power-state of a node.  Also changed the IpmiController so that it returns an array with a true/false flag plus a status message for the "action" commands that it provides (power_on, power_off, power_cycle, power_reset).  This lets the caller differentiate between a node being left in it's existing state and a node being transitioned to a new state, even if the existing state and the new state are the same (attempting to power-on a node that is already powered-on, for example).

--- lib/project_razor/slice/bmc.rb
+++ lib/project_razor/slice/bmc.rb
@@ -20,11 +20,37 @@ module ProjectRazor
         # Here we create a hash of the command string to the method it corresponds to for routing.
         @slice_commands = {:register => "register_bmc",
                            :get => "query_bmc",
+                           :power_on => "power_on_bmc",
+                           :power_off => "power_off_bmc",
+                           :power_cycle => "power_cycle_bmc",
+                           :power_reset => "power_reset_bmc",
+                           :power_status => "power_status_bmc",
+                           :bmc_info => "bmc_info_bmc",
+                           :bmc_getenables => "bmc_getenables_bmc",
+                           :bmc_guid => "bmc_guid_bmc",
+                           :chassis_status => "chassis_status_bmc",
+                           :lan_print => "lan_print_bmc",
+                           :fru_print => "fru_print_bmc",
                            :default => "query_bmc"}
         @slice_commands_help = {:register => "bmc register (JSON STRING)",
                                 :get => "bmc [get] (JSON STRING)",
+                                :power_on => "bmc power_on (JSON STRING)",
+                                :power_off => "bmc power_off (JSON STRING)",
+                                :power_cycle => "bmc power_cycle (JSON STRING)",
+                                :power_reset => "bmc power_reset (JSON STRING)",
+                                :power_status => "power_status_bmc (JSON STRING)",
+                                :bmc_info => "bmc_info_bmc (JSON STRING)",
+                                :bmc_getenables => "bmc_getenables_bmc (JSON STRING)",
+                                :bmc_guid => "bmc_guid_bmc (JSON STRING)",
+                                :chassis_status => "chassis_status_bmc (JSON STRING)",
+                                :lan_print => "lan_print_bmc (JSON STRING)",
+                                :fru_print => "fru_print_bmc (JSON STRING)",
                                 :default => "bmc [get] (JSON STRING)"}
         @slice_name = "Bmc"
+        @data = ProjectRazor::Data.new
+        config = @data.config
+        @ipmi_username = config.default_ipmi_username
+        @ipmi_password = config.default_ipmi_password
       end
 
 
@@ -64,6 +90,116 @@ module ProjectRazor
         end
       end
 
+      # Power on a node (that is already off)
+      def power_on_bmc
+        logger.debug "Power_On bmc called"
+        @command_name = "power_on_bmc"
+        change_bmc_power_state("on")
+      end
+
+      # Power off a node (that is already on)
+      def power_off_bmc
+        logger.debug "Power_On bmc called"
+        @command_name = "power_off_bmc"
+        change_bmc_power_state("off")
+      end
+
+      # Power-cycle a node (that is already on)
+      def power_cycle_bmc
+        logger.debug "Power_On bmc called"
+        @command_name = "power_cycle_bmc"
+        change_bmc_power_state("cycle")
+      end
+
+      # Perform a hard-reset on a node (that is already on)
+      def power_reset_bmc
+        logger.debug "Power_On bmc called"
+        @command_name = "power_reset_bmc"
+        change_bmc_power_state("reset")
+      end
+
+      # Handler for changing power state of a bmc node to a new state
+      # @param [String] new_state
+      #
+      # Possible values for the new_state parameter are as follows:
+      #
+      #     "on"    =>  Powers on the node
+      #     "off"   =>  Powers off the node
+      #     "cycle" => Power cycles the node
+      #     "reset" => Performs a hard reset of the node
+      #
+      # This method is meant to be invoked from the various commands provided by
+      # the slice to change the power-state of the node
+      def change_bmc_power_state(new_state)
+        if @web_command
+          @command_query_string = @command_array.shift
+          if @command_query_string == "{}"
+            logger.error "Missing bmc details"
+            slice_error("MissingDetails")
+          else
+            begin
+              details = JSON.parse(@command_query_string)
+              if details['@uuid'] != nil
+                logger.debug "Changing power-state of bmc: #{details['@uuid']} to #{new_state}"
+                details['@timestamp'] = Time.now.to_i
+                bmc = get_bmc(details)
+                power_state_changed, status_string = bmc.change_power_state(@ipmi_username, @ipmi_password)
+                # handle the returned values; how the returned values should be handled will vary
+                # depending on the "new_state" that the node is being transitioned into.  For example,
+                # you can only turn on a node that is off (or turn off a node that is on), but it
+                # isn't an error to turn on a node that is already on (or turn off a node that is
+                # already off) since that operation is, in effect, a no-op.  On the other hand,
+                # it is an error to try to power-cycle or reset a node that isn't already on, since
+                # these operations don't make any sense on a powered-off node.
+                case new_state
+                  when new_state = "on"
+                    if power_state_changed && /Up\/On/.matches(status_string)
+                      slice_success("Node #{details['@uuid']} now powering on", false)
+                    elsif !power_state_changed && /Up\/On/.matches(status_string)
+                      slice_success("Node #{details['@uuid']} already powered on", false)
+                    else
+                      logger.error "Could not power on bmc"
+                      slice_error("CouldNotPowerOn", false)
+                    end
+                  when new_state = "off"
+                    if power_state_changed && /Down\/Off/.matches(status_string)
+                      slice_success("Node #{details['@uuid']} now powering off", false)
+                    elsif !power_state_changed && /Down\/Off/.matches(status_string)
+                      slice_success("Node #{details['@uuid']} already powered off", false)
+                    else
+                      logger.error "Could not power off bmc"
+                      slice_error("CouldNotPowerOff", false)
+                    end
+                  when new_state = "cycle"
+                    if power_state_changed && /Cycle/.matches(status_string)
+                      slice_success("Node #{details['@uuid']} now power cycling", false)
+                    elsif !power_state_changed && /Off/.matches(status_string)
+                      slice_error("Node #{details['@uuid']} powered off, cannot power cycle", false)
+                    else
+                      logger.error "Could not power off bmc"
+                      slice_error("CouldNotPowerOff", false)
+                    end
+                  when new_state = "reset"
+                    if power_state_changed && /Reset/.matches(status_string)
+                      slice_success("Node #{details['@uuid']} now powering off", false)
+                    elsif !power_state_changed && /Off/.matches(status_string)
+                      slice_error("Node #{details['@uuid']} powered off, cannot reset", false)
+                    else
+                      logger.error "Could not power off bmc"
+                      slice_error("CouldNotPowerOff", false)
+                    end
+                end
+              else
+                logger.error "Incomplete bmc details"
+                slice_error("IncompleteDetails", false)
+              end
+            rescue StandardError => e
+              slice_error(e.message, false)
+            end
+          end
+        end
+      end
+
       # Inserts bmc using hash
       # @param [Hash] bmc_hash
       # @return [ProjectRazor::Bmc]
@@ -78,6 +214,11 @@ module ProjectRazor
         else
           @data.persist_object(ProjectRazor::PowerControl::Bmc.new(bmc_hash))
         end
+      end
+
+      def get_bmc(bmc_hash)
+        setup_data
+        existing_bmc = @data.fetch_object_by_uuid(:bmc, bmc_hash['@uuid'])
       end
 
       def query_bmc
