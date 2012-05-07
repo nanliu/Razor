6fe2f10977b898884d8b5f565ee2bbfa856f5efe
Tom McSweeney <tjmcs@coastaccess.com>
2012-05-07 12:11:41 -0700

    Removed mk_ext_mirror_port from the configuration since it's not used externally...only to construct the mk_ext_mirror_uri.  As such, we're leaving it in place as a local variable but not keeping it in the configuration file itself.

--- lib/project_razor/config/server.rb
+++ lib/project_razor/config/server.rb
@@ -24,7 +24,6 @@ module ProjectRazor
       attr_accessor :admin_port
       attr_accessor :api_port
       attr_accessor :image_svc_port
-      attr_accessor :mk_ext_mirror_port
 
       attr_accessor :mk_checkin_interval
       attr_accessor :mk_checkin_skew
@@ -63,7 +62,7 @@ module ProjectRazor
         @admin_port = 8025
         @api_port = 8026
         @image_svc_port = 8027
-        @mk_ext_mirror_port = 8028
+        mk_ext_mirror_port = 8028
 
         @mk_checkin_interval = 60
         @mk_checkin_skew = 5
@@ -76,7 +75,7 @@ module ProjectRazor
                                    "(^timezone$)", "(^uniqueid$)", "(^uptime.*$)","(.*json_str$)"]
         @mk_fact_excl_pattern = fact_excl_pattern_array.join("|")
         @mk_log_level = "Logger::ERROR"
-        @mk_ext_mirror_uri = "http://#{get_an_ip}:#{@mk_ext_mirror_port}/tinycorelinux"
+        @mk_ext_mirror_uri = "http://#{get_an_ip}:#{mk_ext_mirror_port}/tinycorelinux"
         @mk_ext_list_uri = @mk_ext_mirror_uri + "/tcl-ext-list.yaml"
 
         @image_svc_path = $img_svc_path
