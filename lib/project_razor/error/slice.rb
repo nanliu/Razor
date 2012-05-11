92f2b64982f2bcfd25b7990c7ef121c2b4f7d015
Nan Liu <nan@puppetlabs.com>
2012-05-11 07:31:05 -0700

    Update the correct slice.

--- lib/project_razor/error/slice.rb
+++ lib/project_razor/error/slice.rb
@@ -8,7 +8,18 @@ require_rel "slice/"
 module ProjectRazor
   module Error
     module Slice
-      #
+      @@error_number = 100
+
+      # We use this to automatically generate self incrementing error numbers.
+      def self.error_number
+        @@error_number += 1
+      end
+
+      ProjectRazor::Error.create_class('InputError', {} ,'', 'ProjectRazor::Error::Slice::Generic')
+      ProjectRazor::Error.create_class('InvalidPlugin', {} ,'', 'ProjectRazor::Error::Slice::Generic')
+      ProjectRazor::Error.create_class('InvalidTemplate', {} ,'', 'ProjectRazor::Error::Slice::Generic')
+      ProjectRazor::Error.create_class('MissingArgument', {} ,'', 'ProjectRazor::Error::Slice::Generic')
+      ProjectRazor::Error.create_class('NotFound', {'@http_err'=>:not_found} ,'', 'ProjectRazor::Error::Slice::Generic')
     end
   end
 end
