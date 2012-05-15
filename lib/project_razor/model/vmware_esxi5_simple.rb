e885de28d9afeb2789a340dd1c73446ac476c06d
Nicholas Weaver <lynxbat@gmail.com>
2012-05-15 15:47:43 -0700

    Small ESXi Model update

--- lib/project_razor/model/vmware_esxi5_simple.rb
+++ lib/project_razor/model/vmware_esxi5_simple.rb
@@ -33,6 +33,13 @@ module ProjectRazor
         @template = :vmware_hypervisor
         @name = "vmware_esxi5_simple"
         @description = "VMware ESXi 5 Simple Deployment"
+        @req_metadata_hash = {
+            "@esx_license" => {:default => "",
+                               :example => "AAAAA-BBBBB-CCCCC-DDDDD-EEEEE",
+                               :validation => '^[A-Z\d]{5}-[A-Z\d]{5}-[A-Z\d]{5}-[A-Z\d]{5}-[A-Z\d]{5}$',
+                               :required => true,
+                               :description => "ESX License Key"}
+        }
 
         # Metadata vars
         esx_license = nil
@@ -51,15 +58,15 @@ module ProjectRazor
         from_hash(hash) unless hash == nil
       end
 
-      def req_metadata_hash
-        {
-            "@esx_license" => {:default => "",
-                               :example => "AAAAA-BBBBB-CCCCC-DDDDD-EEEEE",
-                               :validation => '^[A-Z\d]{5}-[A-Z\d]{5}-[A-Z\d]{5}-[A-Z\d]{5}-[A-Z\d]{5}$',
-                               :required => true,
-                               :description => "ESX License Key"}
-        }
-      end
+      #def req_metadata_hash
+      #  {
+      #      "@esx_license" => {:default => "",
+      #                         :example => "AAAAA-BBBBB-CCCCC-DDDDD-EEEEE",
+      #                         :validation => '^[A-Z\d]{5}-[A-Z\d]{5}-[A-Z\d]{5}-[A-Z\d]{5}-[A-Z\d]{5}$',
+      #                         :required => true,
+      #                         :description => "ESX License Key"}
+      #  }
+      #end
 
       def callback
         {"boot_cfg" => :boot_cfg,
