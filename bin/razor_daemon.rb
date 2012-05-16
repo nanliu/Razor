6ddcfa7d957e16614a81a20b381263ac48b9a6ca
Tom McSweeney <tjmcs@coastaccess.com>
2012-05-16 07:54:40 -0700

    This commit represents v1.0 of our Razor daemon.  It cycles through an event handling loop (with a cycle time set using a parameter from the Razor server configuration file) and during that event handling loop performs three actions (two of which are currently implemented).  First, it checks to ensure that the database instances needed by the Razor server is accessible using the existing Razor server configuration (if it is not accessible, the daemon exits with an error condition, shutting down any existing Razor-related processes as it does so).  Next, it checks to make sure that the Razor-related processes (currently two Node.js instances) are still up and running.  If any of them have failed since the last pass through the event-handling loop, they are restarted.  Finally, it calls a method in the RazorDaemon singleton that is intended to be used to call an underlying slice command (currently unimplemented) that will check the timing of tasks running in the Razor server.  Output of this daemon (and the Node.js instances that it managed) are all captured in the "${RAZOR_HOME}/log/razor_daemon.output" file.  We will continue to dig and see if we can redirect this output to a "razor_daemon.log" file instead, but for now this will work.

--- bin/razor_daemon.rb
+++ bin/razor_daemon.rb
@@ -17,61 +17,167 @@ require 'rubygems'
 require 'daemons'
 
 # add our the Razor lib path to the load path. This is for non-gem ease of use
-current_dir = File.dirname(File.expand_path(__FILE__))
-
-lib_path = current_dir.sub(/\/bin$/,"/lib")
+bin_dir = File.dirname(File.expand_path(__FILE__))
+lib_path = bin_dir.sub(/\/bin$/,"/lib")
 $LOAD_PATH.unshift(lib_path)
 # We require the root lib
 require 'project_razor'
 
+# used in cases where the Razor server configuration file does not have a
+# parameter value for the daemon_min_cycle_time
+DEFAULT_MIN_CYCLE_TIME = 30
+
+# This singleton class is used by the daemon (below) to interact with the
+# underlying Razor (obtaining a copy of the Razor server configuration and
+# managing the underlying services that make up the Razor server).  It is
+# also used to verify that services critical to Razor are running (the daemon
+# will actually throw an error and shut down the Razor server if these
+# critical services are found to be missing during a pass through the
+# daemon's event-handling loop)
 class RazorDaemon < ProjectRazor::Object
   include Singleton
 
+  BIN_DIR = File.dirname(File.expand_path(__FILE__))
+  NODE_COMMAND = '/usr/bin/node'
+  NODE_INSTANCE_NAMES = %W[api.js image_svc.js]
+  NODE_COMMAND_MAP = { 'api.js' => "#{NODE_COMMAND} #{BIN_DIR}/api.js",
+                       'image_svc.js' => "#{NODE_COMMAND} #{BIN_DIR}/image_svc.js"}
+
+  # used to obtain a copy of the Razor configuration
   def get_config
     get_data.config
   end
 
-  def shutdown_instances
-    # clean up before exiting
-    puts "Shut down Node.js instances before exiting..."
+  # used to fire off a task (via a slice command) to the underlying Razor server
+  # instance that checks the timing of long-running processes in the Razor server
+  # (and that handles any that are found to have exceeded their defined time-outs).
+  #
+  # TODO; implement the RazorDaemon.check_task_timing method (and the slice it uses)
+  def check_task_timing
+    # no-op
+  end
+
+  # check the connection with the underlying database.  If no database can be found
+  # (i.e. if a connection does not exist and cannot be established), then an error
+  # will be thrown by this method.
+  def check_database_connection
+    persist_ctrl = get_data.setup_persist
+    raise RuntimeError.new("Database connection could not be established " +
+                               "using the current server configuration; check configuration" +
+                               "and database state") unless (persist_ctrl && persist_ctrl.check_connection)
+  end
+
+  # used during the daemon's event-handling loop to verify that all of the nodes
+  # listed (by name) in the 'NODE_INSTANCE_NAMES' array are still running.  If any
+  # of the named node instances have failed (or were never started), this method
+  # will (re)start those instances
+  def ensure_nodes_running
+    node_proc_info = get_node_instance_info
+    # if there are existing node processes, only restart the nodes that aren't running;
+    # else need to (re)start all of the node instances (because none are running)
+    if node_proc_info.size > 0
+      NODE_INSTANCE_NAMES.each { |node|
+        unless node_proc_info.key?(node)
+          puts "(Re)starting '#{node}' using command '#{NODE_COMMAND_MAP[node]}'"
+          job = fork do
+            exec NODE_COMMAND_MAP[node]
+          end
+          Process.detach(job)
+        end
+      }
+    else
+      NODE_INSTANCE_NAMES.each { |node|
+        puts "(Re)starting '#{node}' using command '#{NODE_COMMAND_MAP[node]}'"
+        job = fork do
+          exec NODE_COMMAND_MAP[node]
+        end
+        Process.detach(job)
+      }
+    end
+  end
+
+  # used to shut down all "node-related" processes in the system during
+  # the process of shutting down this daemon
+  def shutdown_node_instances
+    puts "Shutting down node instances using command 'killall -2 node'"
+    %x[killall -2 node]
+  end
+
+  private
+
+  # returns the information about any "node-related" processes from the
+  # system's process table
+  def get_node_instance_info
+    node_ps_out = %x[ps ax | grep node].split("\n")
+    node_proc_info = {}
+    node_ps_out.each { |line|
+      fields = line.split
+      NODE_INSTANCE_NAMES.each { |node_name|
+        node_proc_info[node_name] = fields if (fields.count { |field|
+          /bin\/#{node_name}$/.match(field)} > 0)
+      }
+    }
+    node_proc_info
   end
 
 end
 
 # define the directory to use for logging
-log_dir = current_dir.sub(/\/bin$/,"/log")
+log_dir = bin_dir.sub(/\/bin$/,"/log")
 
 # used to cleanly shut down the processes being managed by this daemon
 # (i.e. the Node.js instances)
 def shutdown_instances
   # clean up before exiting
-  razor_controller = RazorDaemon.instance
-  razor_controller.shutdown_instances
+  razor_daemon = RazorDaemon.instance
+  razor_daemon.shutdown_node_instances
 end
 
+# used to get the minimum cycle time from the Razor server configuration
+# (the one and only input argument to the function)
+def get_min_cycle_time(razor_config)
+  # get the value that should be used from the Razor server configuration
+  min_cycle_time = razor_config.daemon_min_cycle_time
+  # set to the default value if there was no value read from the configuration
+  min_cycle_time = DEFAULT_MIN_CYCLE_TIME unless min_cycle_time
+  # and return the result
+  min_cycle_time
+end
+
+# define some options for our daemon process (below)
 options = {
     :ontop      => false,
     :multiple => false,
     :log_dir  => log_dir,
-    :backtrace  => true,
     :log_output => true,
-    :stop_proc => shutdown_instances
+    :backtrace  => true
 }
 
+# and start that daemon process
 Daemons.run_proc("razor_daemon", options) {
 
-  # get a reference to the RazorDaemon instance
-  razor_controller = RazorDaemon.instance
-  razor_config = razor_controller.get_config
+  # used to clean up underlying processes on exit from the daemon process
+  at_exit do
+    shutdown_instances
+  end
 
-  # convert the sleep time to milliseconds (for calculation of time remaining in
-  # each iteration; this time will be accurate to the nearest millisecond)
-  msecs_sleep = razor_config.daemon_min_cycle_time * 1000;
+  # get a reference to the RazorDaemon singleton (defined above)
+  razor_daemon = RazorDaemon.instance
 
-  # flag that is used to ensure razor_config is reloaded before each pass through the loop
+  # using that singleton, obtain a copy of the Razor server configuration and,
+  # from that configuration, determine how long to sleep between passes through
+  # the daemon's event-handling loop (below).  Convert the "minimum cycle time"
+  # from that Razor server configuration to milliseconds (for calculation of the
+  # time remaining in each iteration).  This will ensure that the sleep time
+  # for each iteration will be accurate to the nearest millisecond.
+  razor_config = razor_daemon.get_config
+  msecs_sleep = get_min_cycle_time(razor_config) * 1000;
+
+  # flag that is used to ensure razor_config is reloaded before each pass through
+  # the event-handling loop, but not on the first pass (since we just loaded it)
   is_first_iteration = true
 
-  # and enter the main event-handling loop
+  # now that everything is configured properly, enter the main event-handling loop
   loop do
 
     begin
@@ -82,24 +188,27 @@ Daemons.run_proc("razor_daemon", options) {
       t1 = Time.now
 
       # reload configuration from Razor if not the first time through the loop
-      # (in which case the razor_config variable will be set to nil)
       unless is_first_iteration
-        razor_config = razor_controller.get_config
+        razor_config = razor_daemon.get_config
         # adjust time to sleep it has changed since the last iteration
-        msecs_sleep = razor_config.daemon_min_cycle_time * 1000;
+        msecs_sleep = get_min_cycle_time(razor_config) * 1000;
       else
         is_first_iteration = false
       end
 
-      # check that instances of critical services (Node.js and mongodb) are running
-      puts "Check that Node.js instances are running and (re)start if needed"
-      sleep(5)
-      puts "Check that mongodb instance is running and throw an error (and exit) if it is not"
-      sleep(2)
+      # check the connection with the underlying database (if a connection cannot be
+      # established using the current server configuration, an error is thrown by
+      # this method and the daemon will exit (killing the underlying Razor server
+      # processes as it does so).
+      razor_daemon.check_database_connection
 
-      # fire off an event (via a slice command?) to check timings on tasks
-      puts "Fire off event to check timings"
-      sleep(1)
+      # check that instances of critical services (Node.js and mongodb) are running
+      razor_daemon.ensure_nodes_running
+
+      # use the singleton to fire off an event to the Razor server (via a slice
+      # command?) that checks the timings for tasks running in that server.
+      razor_daemon.check_task_timing
+
 
       # check to see how much time has elapsed, sleep for the time remaining
       # in the msecs_sleep time window
@@ -107,11 +216,18 @@ Daemons.run_proc("razor_daemon", options) {
       msecs_elapsed = (t2 - t1) * 1000
       if msecs_elapsed < msecs_sleep then
         secs_sleep = (msecs_sleep - msecs_elapsed)/1000.0
-        puts "Time remaining: #{secs_sleep} seconds..."
+        puts "Sleeping for #{secs_sleep} secs..."
         sleep(secs_sleep) if secs_sleep >= 0.0
       end
 
+    rescue RuntimeError => e
+
+      puts e.message
+      puts "Razor server exiting"
+      exit(-1)
+
     rescue => e
+
       puts "An exception occurred: #{e.message}"
       # check to see how much time has elapsed, sleep for the time remaining
       # in the msecs_sleep time window (to avoid spinning through this loop
@@ -121,10 +237,11 @@ Daemons.run_proc("razor_daemon", options) {
       msecs_elapsed = (t2 - t1) * 1000
       if msecs_elapsed < msecs_sleep then
         secs_sleep = (msecs_sleep - msecs_elapsed)/1000.0
-        puts "Time remaining: #{secs_sleep} seconds..."
         sleep(secs_sleep) if secs_sleep >= 0.0
       end
+
     end
 
   end
+
 }
